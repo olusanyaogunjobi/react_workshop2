71a5a07457a15e0c98024c46ce25e315ed4ba5be
deltagare <iogunjobi@gmail.com>
2021-11-18 22:40:05 +0100

    install 3 party apps

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import react from "react";
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<b>Message</b> , document.getElementById('root') );
+
+
+
+
+
+
+
+
+ReactDOM.render(<b>hello</b> , document.getElementById('root') );
 
 
 
